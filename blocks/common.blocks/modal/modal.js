4f086dc49e4579de7444df36cf01673a21bd6bf0
AlbShar <sharipov.albert1991@gmail.com>
2022-05-04 20:36:05 +0500

    change modal.js

--- blocks/common.blocks/modal/modal.js
+++ blocks/common.blocks/modal/modal.js
@@ -1,14 +1,18 @@
 let modal = document.querySelector('#modal');
-
+let links = document.querySelectorAll('.card__photo > a');
 
 function closeModal(event) {
     if (event.target == modal) {
+        for (let i = 0; i < links.length; i++) {
+            links[i].href = '';
+            links[i].target = '';
+        }
         modal.style.display = 'none';
+
     }
 }
 
 window.addEventListener('click', closeModal);
-window.addEventListener('touchstart', closeModal);
 window.addEventListener('touchend', closeModal);
 window.addEventListener('touchcancel', closeModal);
 window.addEventListener('touchmove', closeModal);
