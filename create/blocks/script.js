76f72a6a5be981170fefc6224e121d4af1c9bd71
AlbShar <sharipov.albert1991@gmail.com>
2022-05-05 18:20:56 +0500

    correct password validation

--- create/blocks/script.js
+++ create/blocks/script.js
@@ -3,6 +3,7 @@ let eye1 = document.querySelectorAll('i#eye')[0];
 let eye2 = document.querySelectorAll('i#eye')[1];
 let pwdInp1 = document.querySelector('#pwd');
 let pwdInp2 = document.querySelector('#pwd2');
+let btn = document.querySelector('#btn');
 
 let letter = document.querySelector("#lowLetter");
 let capital = document.querySelector("#capLetter");
@@ -18,7 +19,8 @@ pwdInp1.onfocus = () => {
 
 pwdInp1.onblur = () => {
     message.classList.remove('signup__message_visible');
-    message.classList.add('signup__message_invisible');
+    message.classList.add('signup__message_invisible');    
+    
 };
 
 pwdInp1.onkeyup = () => {
@@ -29,47 +31,59 @@ pwdInp1.onkeyup = () => {
     if (val.length > 8) {
         length.classList.remove('signup__message-item_invalid');
         length.classList.add('signup__message-item_valid');
-    } /*else {
+    } else {
         length.classList.remove('signup__message-item_valid');
         length.classList.add('signup__message-item_invalid');    
-    }*/
+    }
 
     for (let item of letters) {
         if (val.includes(item)) {
             letter.classList.remove('signup__message-item_invalid');
             letter.classList.add('signup__message-item_valid');
-        } /*else {
+            break;
+        } else {
             letter.classList.remove('signup__message-item_valid');
             letter.classList.add('signup__message-item_invalid');
-        }*/
+        }
     }
 
     for (let item of letters) {
         if (val.includes(item.toUpperCase())) {
             capital.classList.remove('signup__message-item_invalid');
             capital.classList.add('signup__message-item_valid');
-        } /*else {
+            break;
+        } else {
             capital.classList.remove('signup__message-item_valid');
             capital.classList.add('signup__message-item_invalid');
-        }*/
+        }
     }
 
     for (let item of numbers) {
         if (val.includes(item)) {
             number.classList.remove('signup__message-item_invalid');
             number.classList.add('signup__message-item_valid');
-        } /*else {
+            break;
+        } else {
             number.classList.remove('signup__message-item_valid');
             number.classList.add('signup__message-item_invalid');
-        }*/
+        }
     }
-
 };
+
+pwdInp2.onkeyup = () => {
+    if (pwdInp2.value !== pwdInp1.value) {
+        pwdInp2.style.border = '1px solid red';
+        btn.disabled = true;
+    } else {
+        pwdInp2.style.border = '1px solid green';
+        btn.disabled = false;
+    }
+}
 
 function showPwd(event) {
     if (event.target === eye1) {
         eye1.className = (eye1.className ==='fa fa-eye-slash') ? "fa fa-eye": 'fa fa-eye-slash';
-        pwdInp1.type = (pwdInp1.type === "password") ? "text" : "password";    
+        pwdInp1.type = (pwdInp1.type === "password") ? "text" : "password";
     } else {
         eye2.className = (eye2.className ==='fa fa-eye-slash') ? "fa fa-eye": 'fa fa-eye-slash';
         pwdInp2.type = (pwdInp2.type === "password") ? "text" : "password";    
