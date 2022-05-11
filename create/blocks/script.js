aadc5e3c1d4f33b0ab4cc802b2fb010f06688b53
AlbShar <sharipov.albert1991@gmail.com>
2022-05-11 18:58:38 +0500

    add error message in signup form

--- create/blocks/script.js
+++ create/blocks/script.js
@@ -3,6 +3,7 @@ let pwdInp1 = document.querySelector('#pwd');
 let pwdInp2 = document.querySelector('#pwd2');
 let btn = document.querySelector('#btn');
 let ul = document.querySelector('.signup__list');
+let dateBirth = document.querySelector("#dateBirth");
 
 let letter = document.querySelector("#lowLetter");
 let capital = document.querySelector("#capLetter");
@@ -93,18 +94,24 @@ btn.onclick = (event) => {
         pwdInp2.style.border = '1px solid red';
         btn.disabled = true;
         btn.classList.add('signup__submit_disabled');
-        
+
+        let div = `<div id="error" style="color: red;margin: -10px 0 15px 2px;"> Разные пароли. Проверьте пароль </div>`;
+        dateBirth.insertAdjacentHTML('beforebegin', div);       
+
         pwdInp2.onkeyup = () => {
             if (pwdInp2.value == pwdInp1.value) {
                 pwdInp2.style.border = '1px solid green';
                 btn.disabled = false;
                 btn.classList.remove('signup__submit_disabled');
                 btn.classList.add('signup__submit_enable');
+                document.querySelector('#error').hidden = true;
             } else {
                 pwdInp2.style.border = '1px solid red';
                 btn.disabled = true;
                 btn.classList.remove('signup__submit_enable');
                 btn.classList.add('signup__submit_disabled');
+                document.querySelector('#error').hidden = false;
+
             }
         }
     }
