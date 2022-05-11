07a332a47a26a01c82542b8f71bd81f552d9807e
AlbShar <sharipov.albert1991@gmail.com>
2022-05-11 22:04:44 +0500

    correct structure of the project

--- create/blocks/signup/__input/signup__input.js
+++ create/blocks/signup/__input/signup__input.js
@@ -1,26 +1,82 @@
-let eye1 = document.querySelectorAll('i#eye')[0];
-let eye2 = document.querySelectorAll('i#eye')[1];
-let pwdInp1 = document.querySelector('#pwd');
-let pwdInp2 = document.querySelector('#pwd2');
+pwdInp1.onfocus = () => {
+    message.classList.remove('signup__message_invisible');
+    message.classList.add('signup__message_visible');
+};
+
+pwdInp1.onblur = () => {
+    message.classList.remove('signup__message_visible');
+    message.classList.add('signup__message_invisible');
+};
+
+pwdInp1.onkeyup = () => {
+    let letters = 'abcdefghijklmnopqrstuvwxyz';
+    let numbers = '0123456789';
+    let val = pwdInp1.value;
+
+    if (val.length >= 8) {
+        length.classList.remove('signup__message-item_invalid');
+        length.classList.add('signup__message-item_valid');
+    } else {
+        length.classList.remove('signup__message-item_valid');
+        length.classList.add('signup__message-item_invalid');    
+    }
+
+    for (let item of letters) {
+        if (val.includes(item)) {
+            letter.classList.remove('signup__message-item_invalid');
+            letter.classList.add('signup__message-item_valid');
+            break;
+        } else {
+            letter.classList.remove('signup__message-item_valid');
+            letter.classList.add('signup__message-item_invalid');
+        }
+    }
+
+    for (let item of letters) {
+        if (val.includes(item.toUpperCase())) {
+            capital.classList.remove('signup__message-item_invalid');
+            capital.classList.add('signup__message-item_valid');
+            break;
+        } else {
+            capital.classList.remove('signup__message-item_valid');
+            capital.classList.add('signup__message-item_invalid');
+        }
+    }
+
+    for (let item of numbers) {
+        if (val.includes(item)) {
+            number.classList.remove('signup__message-item_invalid');
+            number.classList.add('signup__message-item_valid');
+            break;
+        } else {
+            number.classList.remove('signup__message-item_valid');
+            number.classList.add('signup__message-item_invalid');
+        }
+    }
+};
 
 function showPwd(event) {
-    if (event.target === eye1) {
-        eye1.className = (eye1.className ==='fa fa-eye-slash') ? "fa fa-eye": 'fa fa-eye-slash';
-        pwdInp1.type = (pwdInp1.type === "password") ? "text" : "password";    
-    } else {
-        eye2.className = (eye2.className ==='fa fa-eye-slash') ? "fa fa-eye": 'fa fa-eye-slash';
-        pwdInp2.type = (pwdInp2.type === "password") ? "text" : "password";    
-    }
+    let evTrgt = event.target;
+    if (evTrgt.classList.contains('signup__icon-eye')) {
+        if (evTrgt.id =='eye1' && (message.classList.contains('signup__message_invisible'))) {
+            message.classList.remove('signup__message_invisible');
+            message.classList.add('signup__message_visible');
+        }
+
+        if (evTrgt.classList.contains('fa-eye-slash')) {
+            evTrgt.classList.remove('fa-eye-slash');
+            evTrgt.classList.add('fa-eye');
+        } else {
+            evTrgt.classList.remove('fa-eye');
+            evTrgt.classList.add('fa-eye-slash');
+        }
+        evTrgt.previousElementSibling.type = (evTrgt.previousElementSibling.type === "password") ? "text" : "password";
+    } 
 }
 
-eye1.addEventListener('click', showPwd);
-eye1.addEventListener('touchstart', showPwd);
-eye1.addEventListener('touchend', showPwd);
-eye1.addEventListener('touchcancel', showPwd);
-eye1.addEventListener('touchmove', showPwd);
 
-eye2.addEventListener('click', showPwd);
-eye2.addEventListener('touchstart', showPwd);
-eye2.addEventListener('touchend', showPwd);
-eye2.addEventListener('touchcancel', showPwd);
-eye2.addEventListener('touchmove', showPwd);
+ul.addEventListener('click', showPwd);
+ul.addEventListener('touchstart', showPwd);
+ul.addEventListener('touchend', showPwd);
+ul.addEventListener('touchcancel', showPwd);
+ul.addEventListener('touchmove', showPwd);
