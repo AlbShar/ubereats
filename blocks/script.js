705f670cf5cafdf7a97cccfc62a21ffdfcbc1a51
AlbShar <sharipov.albert1991@gmail.com>
2022-05-12 16:22:47 +0500

    correct file structure

--- blocks/script.js
+++ blocks/script.js
@@ -1,4 +1,15 @@
 let ul = document.querySelector('#list');
+let modal = document.querySelector('#modal');
+let links = document.querySelectorAll('.card__photo > a');
+
+function closeModal(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+        event.preventDefault();
+    }
+}
+
+
 function changeIcon(event) {
     let evTrgt = event.target;
         if (evTrgt.classList.contains('fa-eye-slash')) {
@@ -18,3 +29,8 @@ ul.addEventListener('touchstart', changeIcon);
 ul.addEventListener('touchend', changeIcon);
 ul.addEventListener('touchcancel', changeIcon);
 ul.addEventListener('touchmove', changeIcon);
+
+window.addEventListener('click', closeModal);
+window.addEventListener('touchend', closeModal);
+window.addEventListener('touchcancel', closeModal);
+window.addEventListener('touchmove', closeModal);
