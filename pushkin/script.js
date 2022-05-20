5264628e1bc03ce14d328200c268c3b403b9890c
AlbShar <sharipov.albert1991@gmail.com>
2022-05-21 00:46:00 +0500

    changed script.js file

--- pushkin/script.js
+++ pushkin/script.js
@@ -6,6 +6,7 @@ const cardSoup = Array.from(document.querySelector('#thirdCard').querySelectorAl
 const cardHotDishes = Array.from(document.querySelector('#forthCard').querySelectorAll('.dish'));
 const cardGarnish = Array.from(document.querySelector('#fifthCard').querySelectorAll('.dish'));
 const cardDessert = Array.from(document.querySelector('#sixCard').querySelectorAll('.dish'));
+let clickEvent = ('click' || 'touchstart' || 'touchend' || 'touchcancel' || 'touchmove');
 
 const clearClassActive = (element, className) => {
     element.find(item => item.classList.remove(`${className}` + '_active'));
@@ -17,30 +18,30 @@ const setClassActive = (element, index, className) => {
 
 
 const checkoutTabs = (item, index) => {
-    item.onclick = () => {
-    clearClassActive(linkItems, linkItems[index].classList[0]);
-    clearClassActive(nameDish, nameDish[index].classList[0]);
-
-    clearClassActive(cardSnack, cardSnack[index].classList[0]);    
-    clearClassActive(cardSalad, cardSalad[index].classList[0]);
-    clearClassActive(cardSoup, cardSoup[index].classList[0]);
-    clearClassActive(cardHotDishes, cardHotDishes[index].classList[0]);
-    clearClassActive(cardGarnish, cardGarnish[index].classList[0]);
-    clearClassActive(cardDessert, cardDessert[index].classList[0]);
-
-    setClassActive(linkItems, index, linkItems[index].classList[0]);
-    setClassActive(nameDish, index, nameDish[index].classList[0]);
-
-    setClassActive(cardSnack, index, cardSnack[index].classList[0]);
-    setClassActive(cardSalad, index, cardSalad[index].classList[0]);
-    setClassActive(cardSoup, index, cardSoup[index].classList[0]);
-    setClassActive(cardHotDishes, index, cardHotDishes[index].classList[0]);
-    setClassActive(cardGarnish, index, cardGarnish[index].classList[0]);
-    setClassActive(cardDessert, index, cardDessert[index].classList[0]);
-
-
-    if (item.classList[1].includes('_active')) {return;}
-    };
+    item.addEventListener(clickEvent, () => {
+        clearClassActive(linkItems, linkItems[index].classList[0]);
+        clearClassActive(nameDish, nameDish[index].classList[0]);
+    
+        clearClassActive(cardSnack, cardSnack[index].classList[0]);    
+        clearClassActive(cardSalad, cardSalad[index].classList[0]);
+        clearClassActive(cardSoup, cardSoup[index].classList[0]);
+        clearClassActive(cardHotDishes, cardHotDishes[index].classList[0]);
+        clearClassActive(cardGarnish, cardGarnish[index].classList[0]);
+        clearClassActive(cardDessert, cardDessert[index].classList[0]);
+    
+        setClassActive(linkItems, index, linkItems[index].classList[0]);
+        setClassActive(nameDish, index, nameDish[index].classList[0]);
+    
+        setClassActive(cardSnack, index, cardSnack[index].classList[0]);
+        setClassActive(cardSalad, index, cardSalad[index].classList[0]);
+        setClassActive(cardSoup, index, cardSoup[index].classList[0]);
+        setClassActive(cardHotDishes, index, cardHotDishes[index].classList[0]);
+        setClassActive(cardGarnish, index, cardGarnish[index].classList[0]);
+        setClassActive(cardDessert, index, cardDessert[index].classList[0]);
+        if (item.classList[1].includes('_active')) {return;}
+    
+    });
 };
+
 
 linkItems.forEach(checkoutTabs);
