83f3d42b86263f5d99183073e4485ce12e2b1c77
Albert <sharipov.albert1991@gmail.com>
2022-07-20 22:21:06 +0300

    add burger menu

---
+++ blocks/common.blocks/burger/burger.js
@@ -0,0 +1,57 @@
+const burger = document.querySelector('#burger');
+const nav = document.querySelector('#menu');
+const navItems = nav.querySelectorAll('[data-about="actions_item"]');
+const basketWrapper = document.querySelector('#basketWrapper');
+const basketText = document.createElement('div');
+const switchText = document.createElement('div');
+const iconRegistr = document.createElement('span');
+const iconSignIn = document.createElement('span');
+const switchWrapper = document.querySelector('#switchWrapper');
+const registWrapper = document.querySelector('#registWrapper');
+const signInWrapper = document.querySelector('#signInWrapper');
+let clickEvent = ('click' || 'touchstart' || 'touchend' || 'touchcancel' || 'touchmove');
+
+
+function toggleBurger() {
+    burger.addEventListener(clickEvent ,() => {
+    document.body.classList.toggle('stop-scroll');
+    burger.classList.toggle('burger_active');
+    nav.classList.toggle('actions_visible');
+
+    basketText.innerHTML = "Корзина";
+    basketText.className = 'actions__basket-text';
+    basketWrapper.append(basketText);
+
+    switchText.innerHTML = "Сменить тему";
+    switchText.className = 'switch__text';
+    switchWrapper.append(switchText);
+
+    iconRegistr.style.transform = 'scale(1.3)';
+    iconRegistr.className = 'fa fa-user-plus';
+    iconRegistr.style.marginRight = '5px';
+    iconRegistr.style.marginLeft = '10px';
+    iconRegistr.style.filter = 'var(--filter)';
+    registWrapper.prepend(iconRegistr);
+
+    iconSignIn.style.transform = 'scale(1.5)';
+    iconSignIn.className = 'fa fa-user-circle';
+    iconSignIn.style.marginLeft = '10px';
+    iconSignIn.style.filter = 'var(--filter)';
+    signInWrapper.prepend(iconSignIn);
+
+});
+    
+    navItems.forEach(el => {
+        el.addEventListener('click', () => {
+            document.body.classList.remove('stop-scroll');
+            burger.classList.remove('burger_active');
+            nav.classList.remove('actions_visible');
+            basketText.remove();
+            switchText.remove();
+            iconRegistr.remove();
+            iconSignIn.remove();
+        });
+    });
+}
+
+export {toggleBurger};
--- blocks/script.js
+++ blocks/script.js
@@ -7,10 +7,12 @@ import { closeLoginForm } from "./common.blocks/form/closeLoginForm.js";
 import { changeTypeInput } from "./common.blocks/form/changeTypeInput.js";
 import { showLoginForm } from "./common.blocks/actions/showLoginForm.js";
 import {changeTheme} from "./common.blocks/switch/switch.js";
+import {toggleBurger} from "./common.blocks/burger/burger.js";
 
 showLoginForm();
 searchCard();
 changeTheme();
+toggleBurger();
 window.addEventListener('click', closeLoginForm);
 window.addEventListener('touchstart', closeLoginForm);
 window.addEventListener('touchend', closeLoginForm);
